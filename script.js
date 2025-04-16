82d47195538a6eab4c159c5819da3d46c9f7f601
Juuso Huuskonen <64224782+JuusoHuu@users.noreply.github.com>
2025-04-16 10:28:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const radioYes = document.getElementById("radioYes");
 const radioNo = document.getElementById("radioNo");
 
-const sisalto = document.getElementById("sisalto");
+const kSisalto = document.getElementById("Ksisalto");
+const eSisalto = document.getElementById("Esisalto");
 const slidebar = document.getElementById("slidebar");
 const allergiat = document.getElementById("allergiat");
 const haku = document.getElementById("haku")
 
 radioYes.addEventListener("change", () => {
   if (radioYes.checked) {
-    sisalto.classList.remove("hidden");
+    kSisalto.classList.remove("hidden");
+    eSisalto.classList.add("hidden")
     slidebar.classList.remove("hidden");
     allergiat.classList.remove("hidden");
     haku.classList.remove("hidden");
@@ -17,10 +19,11 @@ radioYes.addEventListener("change", () => {
 
 radioNo.addEventListener("change", () => {
   if (radioNo.checked) {
-    sisalto.classList.add("hidden");
-    slidebar.classList.add("hidden");
-    allergiat.classList.add("hidden");
-    haku.classList.add("hidden");
+    kSisalto.classList.add("hidden");
+    eSisalto.classList.remove("hidden");
+    slidebar.classList.remove("hidden");
+    allergiat.classList.remove("hidden");
+    haku.classList.remove("hidden");
   }
 });
 
@@ -37,32 +40,6 @@ const navList = document.getElementById('nav-lista');
 toggle.addEventListener('click', () => {
   navList.classList.toggle('show');
 });
-
-const toggleButton = document.getElementById('darkModeToggle');
-
-toggleButton.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
-
-  if (document.body.classList.contains('dark-mode')) {
-    toggleButton.textContent = '☀️';
-    localStorage.setItem('theme', 'dark');
-  } else {
-    toggleButton.textContent = '🌙'; 
-    localStorage.setItem('theme', 'light');
-  }
-});
-
-window.addEventListener('DOMContentLoaded', () => {
-  const savedTheme = localStorage.getItem('theme');
-  if (savedTheme === 'dark') {
-    document.body.classList.add('dark-mode');
-    toggleButton.textContent = '☀️';
-  } else {
-    document.body.classList.remove('dark-mode');
-    toggleButton.textContent = '🌙';
-  }
-});
-
 
 function haeReseptit() {
   // Kun Gemini otetaan käyttöön myöhemmin
