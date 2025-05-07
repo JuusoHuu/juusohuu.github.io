940d865a0100adf58bfb7c575d0e99bb312e992a
Juuso Huuskonen <64224782+JuusoHuu@users.noreply.github.com>
2025-05-07 09:26:55 +0300

    Add files via upload

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:3000"
-    : "juusohuugithubio-production-d7ac.up.railway.app";
+    : "https://juusohuugithubio-production-d7ac.up.railway.app";
