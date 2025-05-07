df006c8e64a954e4612081eb1f911b3e29944f69
Juuso Huuskonen <64224782+JuusoHuu@users.noreply.github.com>
2025-05-07 09:43:30 +0300

    Add files via upload

--- env.js
+++ env.js
@@ -1,4 +1,1 @@
-export const API_BASE_URL =
-  import.meta.env.MODE === "localhost"
-    ? "http://localhost:3000"
-    : "https://juusohuugithubio-production-d7ac.up.railway.app";
+const API_BASE_URL = "https://juusohuugithubio-production-d7ac.up.railway.app";
